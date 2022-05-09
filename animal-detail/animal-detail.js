299ad1699410fdf81108ff7426080eb319349802
austinbhan <austinbhan@outlook.com>
2022-05-09 16:05:02 -0700

    Some CSS Formatting

--- animal-detail/animal-detail.js
+++ animal-detail/animal-detail.js
@@ -10,11 +10,14 @@ const animal = findById(params.get('id'), animals);
 console.log(animal);
 
 const animalName = document.getElementById('animal-name');
+animalName.classList.add('Bob');
 animalName.textContent = animal.name;
+
 
 const animalImage = document.createElement('img');
 animalImage.src = `../assets/${animal.type}.svg`;
 
 const animalSpan = document.createElement('span');
+animalSpan.classList.add('font-size');
 animalSpan.textContent = animal.says;
 animalName.append(animalImage, animalSpan);
