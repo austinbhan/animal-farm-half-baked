e64fcc9a6eb571550410c591eb08f38e8079b19f
austinbhan <austinbhan@outlook.com>
2022-05-09 15:43:15 -0700

    Need to resize animal

--- animal-detail/animal-detail.js
+++ animal-detail/animal-detail.js
@@ -1,8 +1,6 @@
 import { findById } from '../utils.js';
 import { animals } from '../data.js';
 
-import { renderListItem } from '../render-utils.js'; // Will this work?
-
 console.log('hello from animal detail');
 
 const params = new URLSearchParams(window.location.search);
@@ -13,3 +11,11 @@ console.log(animal);
 
 const animalName = document.getElementById('animal-name');
 animalName.textContent = animal.name;
+
+const animalImage = document.createElement('img');
+animalImage.classList.add('animal-pic');
+animalImage.src = `../assets/${animal.type}.svg`;
+
+const animalSpan = document.createElement('span');
+animalSpan.textContent = animal.says;
+animalName.append(animalImage, animalSpan);
