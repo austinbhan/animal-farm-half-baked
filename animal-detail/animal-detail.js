2efff6d90877dfd632179da9d6506a966415edc2
austinbhan <austinbhan@outlook.com>
2022-05-09 14:07:44 -0700

    Try making a button for the image

--- animal-detail/animal-detail.js
+++ animal-detail/animal-detail.js
@@ -1,3 +1,12 @@
 import { findById } from '../utils.js';
 import { animals } from '../data.js';
 console.log('hello from animal detail');
+
+const params = new URLSearchParams(window.location.search);
+console.log(params.get('id'));
+
+const animal = findById(params.get('id'), animals);
+console.log(animal);
+
+const animalName = document.getElementById('animal-name');
+animalName.textContent = animal.name;
